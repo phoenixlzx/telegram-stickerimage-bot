85e254c49dddf2975f2a248203c1df6ff5af1b24
Phoenix Nemo <i@phoenixlzx.com>
2016-08-23 00:55:44 +0800

    switch to jszip. update log message and more

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var path = require('path');
 
 var TelegramBot = require('node-telegram-bot-api');
 var im = require('imagemagick');
-var AdmZip = require('adm-zip');
+var JSZip = require("jszip");
 var async = require('async');
 
 var token = config.token;
@@ -32,7 +32,8 @@ bot.onText(/\/newpack*/i, function (msg) {
     ramdb[chatId] = {
         start: msg.date,
         files: [],
-        srcimg: []
+        srcimg: [],
+        destimg: []
     };
     bot.sendMessage(chatId, messages.msg.newpack.replace('%max%', config.maximages));
 });
@@ -44,8 +45,8 @@ bot.onText(/\/finish\s?(png)?\s?(\d+)?/i, function (msg, match) {
     if (ramdb[chatId] && ramdb[chatId].files.length > 0) {
         console.log('[' + chatId + '] Starting pack task...');
         var packpath = config.file_storage + '/' + chatId,
-            srcpath = packpath + '/src',
-            imgpath = packpath + '/img';
+            srcpath = packpath + '/src/',
+            imgpath = packpath + '/img/';
         fs.mkdirpSync(path.resolve(packpath));
         fs.mkdirpSync(path.resolve(srcpath));
         fs.mkdirpSync(path.resolve(imgpath));
@@ -53,14 +54,14 @@ bot.onText(/\/finish\s?(png)?\s?(\d+)?/i, function (msg, match) {
             function (cb) {
                 console.log('[' + chatId + '] Downloading files...');
                 bot.sendMessage(chatId, messages.msg.downloading);
-                async.eachSeries(ramdb[chatId].files, function(fileId, callback) {
+                async.eachSeries(ramdb[chatId].files, function (fileId, callback) {
                     bot.downloadFile(fileId, path.resolve(srcpath))
                             .then(function (srcimg) {
                                 console.log('[' + chatId + '] File ' + fileId + ' saved to disk.');
                                 ramdb[chatId].srcimg.push(srcimg);
                                 callback();
                             });
-                    }, function(err) {
+                    }, function () {
                         cb();
                     });
             },
@@ -69,43 +70,60 @@ bot.onText(/\/finish\s?(png)?\s?(\d+)?/i, function (msg, match) {
                 bot.sendMessage(chatId, messages.msg.converting);
                 async.eachSeries(ramdb[chatId].srcimg, function (src, callback) {
                     var imarg = [src];
+                    var destimg = path.resolve(imgpath + '/' + path.basename(src, 'webp') + 'jpg');
                     if (width && width < 512) {
                         imarg.push('-resize', width + 'x' + width);
                     }
                     if (format === 'png') {
-                        imarg.push(path.resolve(imgpath + '/' + path.basename(src, 'webp') + 'png'))
+                        destimg = path.resolve(imgpath + '/' + path.basename(src, 'webp') + 'png');
+                        imarg.push(destimg);
                     } else {
                         // use -flatten to add white background to jpg files
-                        imarg.push('-flatten', path.resolve(imgpath + '/' + path.basename(src, 'webp') + 'jpg'))
+                        imarg.push('-flatten', destimg)
                     }
                     console.log('[' + chatId + '] Convert command:', im.convert.path, imarg.join(' '));
                     im.convert(imarg, function (err, stdout) {
-                        if (err) {
-                            // ...
-                        }
-                        console.log(stdout);
+                        ramdb[chatId].destimg.push(destimg);
                         callback(err);
                     });
                 }, function (err) {
+                    if (err) {
+                        return cleanup(chatId);
+                    }
                     cb();
                 });
             },
             function (cb) {
                 bot.sendMessage(chatId, messages.msg.packaging);
+                console.log('[' + chatId + '] Adding files to package...');
+                var zip = new JSZip();
+                ramdb[chatId].srcimg.forEach(function (src) {
+                    var fname = chatId + '/src/' + path.basename(src);
+                    console.log('[' + chatId + '] Adding file ', fname);
+                    zip.file(fname, fs.readFileSync(path.resolve(src)));
+                });
+                ramdb[chatId].destimg.forEach(function (dest) {
+                    var fname = chatId + '/img/' + path.basename(dest);
+                    console.log('[' + chatId + '] Adding file ', fname);
+                    zip.file(fname, fs.readFileSync(path.resolve(dest)));
+                });
                 console.log('[' + chatId + '] Packaging files...');
-                var zip = new AdmZip();
-                zip.addLocalFolder(path.resolve(packpath));
-                var packdata = zip.toBuffer();
-                cb(null, packdata);
+                zip.generateAsync({
+                    compression: 'DEFLATE',
+                    type: 'nodebuffer',
+                    comment: 'Created by stickerimagebot',
+                    platform: process.platform
+                })
+                    .then(function (content) {
+                        cb(null, content);
+                    });
             }],
             function (err, res) {
                 bot.sendMessage(chatId, messages.msg.sending);
                 bot.sendDocument(chatId, res[2]);
                 console.log('[' + chatId + '] Sending zip file...');
                 // clear task
-                console.log('[' + chatId + '] Clearing up...');
-                fs.removeSync(path.resolve(packpath));
-                ramdb[chatId] = undefined;
+                cleanup(chatId);
                 console.log('[' + chatId + '] Task finished.');
             }
         );
@@ -126,3 +144,9 @@ bot.on('message', function (msg) {
         }
     }
 });
+
+function cleanup(id) {
+    console.log('[' + id + '] Clearing up...');
+    ramdb[id] = undefined;
+    fs.removeSync(path.resolve(config.file_storage + '/' + id));
+}
